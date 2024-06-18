cc937bd83272f804b0751a89475ffe41d4991c50
Christian-sauceda <81939528+Christian-sauceda@users.noreply.github.com>
2024-06-18 11:03:59 -0600

    act

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,11 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_DATABASE: process.env.DB_DATABASE,
+  DB_DIALECT: process.env.DB_DIALECT,
+  DB_HOST: process.env.DB_HOST,
 };
 
 export default config;
--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,21 @@
 import Sequelize from 'sequelize';
+import config from '../config/config.js';
+const { DB_USER, DB_PASSWORD, DB_DATABASE, DB_DIALECT, DB_HOST } = config;
 
 export const development = {
-  username: 'root',
-  password: 'root',
-  database: 'app',
-  dialect: 'mysql',
-  host: 'localhost',
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  dialect: DB_DIALECT,
+  host: DB_HOST,
   operatorsAliases: Sequelize.Op,
 };
 
 export const production = {
-  username: 'root',
-  password: 'root',
-  database: 'app',
-  dialect: 'mysql',
-  host: 'localhost',
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  dialect: DB_DIALECT,
+  host: DB_HOST,
   operatorsAliases: Sequelize.Op,
 };
